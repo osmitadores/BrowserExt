c355cc7962caffb4498b07347664997f7251d518
William Claude Tumeo <tumeowilliam@gmail.com>
2015-07-13 04:38:35 -0300

    Teste com criação de elemento

--- Chrome/mythMain.js
+++ Chrome/mythMain.js
@@ -3,10 +3,25 @@ main();
 function main(){
     var saite = window.location.hostname;
     if(saite.search("facebook.com") !== -1){
+        mythBook();
         alert("É o facebook!");
     }else{
         alert("Otro saite!");
     }
     
     return 0;
+}
+
+function mythBook(){
+    
+    var styleNode = [
+        "._5vb_, ._5vb_ #contentCol",
+        "{background-color:black}",
+    ];
+    styleNode = styleNode.join(" ");
+    var addStyle = document.createElement("style");
+    var newStyle = document.createTextNode(styleNode);
+    
+    addStyle.appendChild(newStyle);
+    document.body.appendChild(addStyle);
 }
