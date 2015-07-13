2518e4c9685625c571f68a1cb165ddbde767a626
William Claude Tumeo <tumeowilliam@gmail.com>
2015-07-13 08:39:15 -0300

    Fazendo uns testezinhos

--- Chrome/mythMain.js
+++ Chrome/mythMain.js
@@ -4,59 +4,84 @@ function main(){
     var saite = window.location.hostname;
     if(saite.search("facebook.com") !== -1){
         mythBook();
-     //   alert("É o facebook!");
-    }else{
-    //   # alert("Otro saite!");
-    }
-    
-	  if(saite.search("plug.dj") !== -1){ 
+    }else if(saite.search("plug.dj") !== -1){
 	  mythPlug();
 	  alert("É o plug!");
-	  
-	  }
-	
-	
+  }else {
+      return 1;
+  }
     return 0;
-}  
+}
 
 function mythBook(){
-    
-    var styleNode = [
-        "._5vb_, ._5vb_ #contentCol",
-        "{background-color:black}",
 
+    var x = document.getElementById('q');
+    x.placeholder = "Procure mitos, mitoses e mitarias"; // Texto temporário
+    var j = document.getElementById('findFriendsNav');
+    j.textContent = "Encontrar mitos";
+
+    var styleNode = ['._5lus a {',
+    	'background: url(http://i.imgur.com/GQJa5a7.jpg);',
+        'background-size: cover;',
+        'border-radius: 20%}',
+        '._5vb_, ._5vb_ {',
+        'background-color: #eee}',
+        '._4f7n {',
+        'background: #45484d;',
+        'background: -moz-linear-gradient(top,  #45484d 0%, #000000 100%);',
+        'background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#45484d), color-stop(100%,#000000));',
+        'background: -webkit-linear-gradient(top,  #45484d 0%,#000000 100%);',
+        'background: -o-linear-gradient(top,  #45484d 0%,#000000 100%);',
+        'background: -ms-linear-gradient(top,  #45484d 0%,#000000 100%);',
+        'background: linear-gradient(to bottom,  #45484d 0%,#000000 100%);}',
+        '.loggedout_menubar_container {',
+        'background: #45484d;',
+        'background: -moz-linear-gradient(top,  #45484d 0%, #000000 100%);',
+        'background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#45484d), color-stop(100%,#000000));',
+        'background: -webkit-linear-gradient(top,  #45484d 0%,#000000 100%);',
+        'background: -o-linear-gradient(top,  #45484d 0%,#000000 100%);',
+        'background: -ms-linear-gradient(top,  #45484d 0%,#000000 100%);',
+        'background: linear-gradient(to bottom,  #45484d 0%,#000000 100%);}',
+        '._li {',
+        'background-size: contain;',
+        'background-repeat: repeat-y;}',
+        '.litestandClassicWelcomeBox a {',
+        'color: #FFFFFF;}',
+        '.uiHeader h2 {',
+        'color: #FFFFFF;',
+        'font-size: 16px;}',
+        '.fbIndex .gradient {',
+        'background: none!important;}',
+        '.fbIndex ._li {',
+        'height: 100%;',
+        'background-size: cover;',
+        'background-repeat: repeat-y;}'
     ];
+
     styleNode = styleNode.join(" ");
     var addStyle = document.createElement("style");
     var newStyle = document.createTextNode(styleNode);
-    
+
     addStyle.appendChild(newStyle);
     document.body.appendChild(addStyle);
 }
 
 
- {callback}
-
 function mythPlug(){
-
 
     var styleNode = [
         ".room-background",
         "{background-image:url('http://i.imgur.com/zSMRtE6.png' ) !important ;}",
 				".torch.right, .torch",
 		"{visibility:hidden}",
-		
-		
-		
+
+
+
     ];
     styleNode = styleNode.join(" ");
     var addStyle = document.createElement("style");
     var newStyle = document.createTextNode(styleNode);
-    
+
     addStyle.appendChild(newStyle);
     document.body.appendChild(addStyle);
 }
-
-
-
-
